044af42e12659010355a44a8736ea6d5ff50bd72
anirudhprajeev-create <anirudhprajeev@gmail.com>
2025-10-03 14:46:19 +0530

    commit

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,7 +8,7 @@ import { redirect } from 'next/navigation'
 
 const inquirySchema = z.object({
   inGameName: z.string().min(2, "Ingame name must be at least 2 characters."),
-  inGamePhoneNumber: z.string().optional(),
+  inGamePhoneNumber: z.string().min(5, "Ingame phone number must be at least 5 characters.").optional(),
   message: z.string().min(10, "Message must be at least 10 characters."),
   vehicle: z.string(),
 });
